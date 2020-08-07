bc2d7316bb5297004804f8bb03cc0ce2468f4fd1
Tomasz Rytel <ryteltomasz@gmail.com>
2020-08-07 17:09:54 +0200

    change

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Fib Calculator
           </p>
           <a className="App-link" href="https://reactjs.org"
             target="_blank" rel="noopener noreferrer">

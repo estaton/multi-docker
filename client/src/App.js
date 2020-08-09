29a7a1db44969ad8667dd28abec0f7232b1f7600
Tomasz Rytel <ryteltomasz@gmail.com>
2020-08-09 10:38:46 +0200

    fib v2

--- client/src/App.js
+++ client/src/App.js
@@ -12,12 +12,8 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Fib Calculator
+            Fib Calculator v0.2.0
           </p>
-          <a className="App-link" href="https://reactjs.org"
-            target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
